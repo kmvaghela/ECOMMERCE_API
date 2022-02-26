3f0292e0df2e9ae283626471cde3e530a2c805b1
parthm@avishkaritservices.com <49136674+kmvaghela@users.noreply.github.com>
2022-02-26 18:35:13 +0530

    user route with token verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER 
 router.post("/register", async (req, res) => {
@@ -17,6 +18,36 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
         // console.log(error);
+    }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong Credentials!");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        OriginalPassword !== req.body.password && res.status(401).json("Wrong Credentials!");
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
